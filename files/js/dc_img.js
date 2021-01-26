6ad57fb92b1d1a642b27df3009f774a3c0ddb4c2
nowfindwomen <78023971+nowfindwomen@users.noreply.github.com>
2021-01-26 14:33:52 +0300

    Add files via upload

---
+++ files/js/dc_img.js
@@ -0,0 +1,6 @@
+(function(w,d,s,v,n,p){w['DataCloudEC']=w['DataCloudEC']||[];w[v]=w[v]||function(){(w[v].q=w[v].q||[]).push(arguments);};w[v].l=Date.now();if(~w['DataCloudEC'].indexOf(s))return;n=d.createElement('script');p=d.getElementsByTagName('head')[0];n.async=true;n.charset='utf-8';n.src=s;n.type='text/javascript';p.appendChild(n);w['DataCloudEC'].push(s);})
+(window,document,window._pixel_scr||'','_dct');
+(function(w) {
+var iData=w._ins_opt||{};
+_dct&&_dct(w._pixel_url||'',iData);
+}(window));
